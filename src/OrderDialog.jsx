4e5769ce355a54764aa068e57143e3b0d1fbc442
Helaman Bennion <helama22@byu.edu>
2024-05-24 17:28:26 -0600

    fix total cost to update in real time

--- src/OrderDialog.jsx
+++ src/OrderDialog.jsx
@@ -3,23 +3,35 @@ import {useMountedFetch, fetchTimes} from "./fetch.jsx";
 import { Times } from './times.jsx'
 
 export default function OrderDialog(props) {
+    // console.log(props.menuItems ?? "Sucketh")
+    let [amounts, setAmounts] = React.useState([])
     let [totalCost, setTotalCost] = React.useState(0)
 
     function hideDialog() {
         props.setDialogDisplay(!props.showDialog)
     }
 
-    return(
+    function updateTotalCost(newVal, i) {
+        amounts[i] = parseInt(newVal)
+        let newTotal = amounts.reduce((x, z) => x + z)
+        setAmounts(amounts)
+        setTotalCost(newTotal)
+    }
+
+    return (
         <div>
             {props.showDialog ? (
                 <dialog id="order-dialog" style={{display: 'flex'}}>
                     <span className={"close-button topright"} onClick={hideDialog}>&times;</span>
                     <p id={"order-dialog-header"}>Complete your order</p>
-                    {props.menuItems.map(az => {
+                    {props.menuItems.map((az, i) => {
+                        amounts.length < props.menuItems.length ? amounts.push(0) : ""
                         return(
                             <div className={"order-dialog-option-container"}>
                                 <h3 className={"menu-option-title"} style={az.style}>{az.name}</h3>
-                                <input id={`${az.id}-order-amount`} className={"amount-input"} type={"number"} defaultValue={0} min={0} step={1} />
+                                <input id={`${az.id}-order-amount`} className={"amount-input"} type={"number"}
+                                       defaultValue={0} min={0}
+                                       max={12} step={1} onChange={event => updateTotalCost(event.target.value, i)}/>
                             </div>
                         )
                     })}
@@ -27,9 +39,9 @@ export default function OrderDialog(props) {
                         <p id='time-selection-banner'>
                             Pickup Date and Time
                         </p>
-                        <Times />
+                        <Times/>
                     </div>
-                    <p>Total cost for deliciousness: {}</p>
+                    <p className="total-cost">Total cost for deliciousness: ${totalCost*6}</p>
                     <button onClick=''>Confirm</button>
                 </dialog>
             ) : (<div></div>)}
